df2e9230415182d38b51fea9f4bc69b95cb65816
AkshaySarkale <akshaysarkale999@gmail.com>
2026-05-29 10:14:48 +0530

    added restrictions on random otp text box filled

--- lib/flutter_autofill_otp/autofillOtp.dart
+++ lib/flutter_autofill_otp/autofillOtp.dart
@@ -1,17 +1,23 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
-class Autofillotp extends StatelessWidget {
+class Autofillotp extends StatefulWidget {
   final int numberOfTxtFeilds;
   final Color? borderColor;
 
   const Autofillotp({super.key, required this.numberOfTxtFeilds,this.borderColor});
 
   @override
+  State<Autofillotp> createState() => _AutofillotpState();
+}
+
+class _AutofillotpState extends State<Autofillotp> {
+  late List<TextEditingController> controllers;
+  @override
   Widget build(BuildContext context) {
     return Row(
       mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-      children: List.generate(numberOfTxtFeilds, (index) {
+      children: List.generate(widget.numberOfTxtFeilds, (index) {
         return SizedBox(
           width: 50,
           child: TextField(
@@ -21,16 +27,25 @@ class Autofillotp extends StatelessWidget {
             inputFormatters: [
               FilteringTextInputFormatter.digitsOnly
             ],
+            onTap: () {
+            // prevent skipping empty fields
+            for (int i = 0; i < index; i++) {
+          if (controllers[i].text.isEmpty) {
+            FocusScope.of(context).requestFocus(focusNodes[i]);
+            return;
+          }
+        }
+      },
             decoration: InputDecoration(
               counterText: "",
               border: OutlineInputBorder(
                 borderSide: BorderSide(
-                  color: borderColor ?? Colors.grey
+                  color: widget.borderColor ?? Colors.grey
                 )
               ),
             ),
             onChanged: (value) {
-              if (value.isNotEmpty && index < numberOfTxtFeilds) {
+              if (value.isNotEmpty && index < widget.numberOfTxtFeilds) {
                 FocusScope.of(context).nextFocus();
               }
             },
