0cc17e5767d61015ef1d027156253de14d183858
AkshaySarkale <akshaysarkale999@gmail.com>
2026-05-29 10:02:34 +0530

    added border

--- lib/flutter_autofill_otp/autofillOtp.dart
+++ lib/flutter_autofill_otp/autofillOtp.dart
@@ -1,30 +1,33 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
-class Autofillotp extends StatefulWidget {
+class Autofillotp extends StatelessWidget {
   final int numberOfTxtFeilds;
+  final Color? borderColor;
 
-  const Autofillotp({super.key, required this.numberOfTxtFeilds});
+  const Autofillotp({super.key, required this.numberOfTxtFeilds,this.borderColor});
 
-  @override
-  State<Autofillotp> createState() => _AutofillotpState();
-}
-
-class _AutofillotpState extends State<Autofillotp> {
   @override
   Widget build(BuildContext context) {
     return Row(
       mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-      children: List.generate(widget.numberOfTxtFeilds, (index) {
+      children: List.generate(numberOfTxtFeilds, (index) {
         return SizedBox(
           width: 50,
           child: TextField(
             keyboardType: TextInputType.number,
             textAlign: TextAlign.center,
             maxLength: 1,
-            decoration: const InputDecoration(counterText: ""),
+            decoration: InputDecoration(
+              counterText: "",
+              border: OutlineInputBorder(
+                borderSide: BorderSide(
+                  color: borderColor ?? Colors.grey
+                )
+              ),
+            ),
             onChanged: (value) {
-              if (value.isNotEmpty && index < widget.numberOfTxtFeilds) {
+              if (value.isNotEmpty && index < numberOfTxtFeilds) {
                 FocusScope.of(context).nextFocus();
               }
             },
