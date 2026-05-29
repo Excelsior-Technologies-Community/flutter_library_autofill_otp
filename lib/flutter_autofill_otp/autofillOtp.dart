162f50952cf186c98eed3ee6e2fa5b3753702867
AkshaySarkale <akshaysarkale999@gmail.com>
2026-05-29 10:41:14 +0530

    fixing bugs & add onCompleted and onchanged functionally

--- lib/flutter_autofill_otp/autofillOtp.dart
+++ lib/flutter_autofill_otp/autofillOtp.dart
@@ -1,17 +1,27 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
-class Autofillotp extends StatefulWidget {
+class AutofillOtp extends StatefulWidget {
   final int numberOfTxtFeilds;
   final Color? borderColor;
+  final Function(String otp)? onCompleted;
+  final Function(String otp)? onChanged;
+  final bool autoFocus;
 
-  const Autofillotp({super.key, required this.numberOfTxtFeilds,this.borderColor});
+  const AutofillOtp({
+    super.key,
+    required this.numberOfTxtFeilds,
+    this.borderColor,
+    this.onChanged,
+    this.onCompleted,
+    this.autoFocus = true,
+  });
 
   @override
-  State<Autofillotp> createState() => _AutofillotpState();
+  State<AutofillOtp> createState() => _AutofillOtpState();
 }
 
-class _AutofillotpState extends State<Autofillotp> {
+class _AutofillOtpState extends State<AutofillOtp> {
   late List<TextEditingController> controllers;
   late List<FocusNode> focusNodes;
 
@@ -28,7 +38,16 @@ class _AutofillotpState extends State<Autofillotp> {
       widget.numberOfTxtFeilds,
           (_) => FocusNode(),
     );
+
+    if (widget.autoFocus) {
+      WidgetsBinding.instance.addPostFrameCallback((_) {
+        if (focusNodes.isNotEmpty) {
+          FocusScope.of(context).requestFocus(focusNodes[0]);
+        }
+      });
+    }
   }
+
   @override
   void dispose() {
     for (var c in controllers) {
@@ -40,6 +59,19 @@ class _AutofillotpState extends State<Autofillotp> {
     super.dispose();
   }
 
+  String getOtp() {
+    return controllers.map((e) => e.text).join();
+  }
+
+  void _notify() {
+    final otp = getOtp();
+    widget.onChanged?.call(otp);
+
+    if (!otp.contains("") && otp.length == widget.numberOfTxtFeilds) {
+      widget.onCompleted?.call(otp);
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Row(
@@ -47,48 +79,59 @@ class _AutofillotpState extends State<Autofillotp> {
       children: List.generate(widget.numberOfTxtFeilds, (index) {
         return SizedBox(
           width: 50,
-          child: TextField(
-            controller: controllers[index],
-            focusNode: focusNodes[index],
-            keyboardType: TextInputType.number,
-            textAlign: TextAlign.center,
-            maxLength: 1,
-            inputFormatters: [
-              FilteringTextInputFormatter.digitsOnly
-            ],
-            onTap: () {
-              for (int i = 0; i < index; i++) {
-                if (controllers[i].text.isEmpty) {
-                  FocusScope.of(context).requestFocus(focusNodes[i]);
-                  return;
+          child: Focus(
+            onKeyEvent: (node, event) {
+              if (event is KeyDownEvent &&
+                  event.logicalKey == LogicalKeyboardKey.backspace) {
+                if (controllers[index].text.isEmpty && index > 0) {
+                  FocusScope.of(context)
+                      .requestFocus(focusNodes[index - 1]);
                 }
               }
+              return KeyEventResult.ignored;
             },
+            child: TextField(
+              controller: controllers[index],
+              focusNode: focusNodes[index],
+              keyboardType: TextInputType.number,
+              textAlign: TextAlign.center,
+              maxLength: 1,
+              inputFormatters: [
+                FilteringTextInputFormatter.digitsOnly,
+                LengthLimitingTextInputFormatter(1),
+              ],
 
-            decoration: InputDecoration(
-              counterText: "",
-              border: OutlineInputBorder(
-                borderSide: BorderSide(
-                  color: widget.borderColor ?? Colors.grey,
+              decoration: InputDecoration(
+                counterText: "",
+                border: OutlineInputBorder(
+                  borderSide: BorderSide(
+                    color: widget.borderColor ?? Colors.grey,
+                  ),
                 ),
               ),
-            ),
 
-            onChanged: (value) {
-              // move back
-              if (value.isEmpty && index > 0) {
-                FocusScope.of(context).requestFocus(focusNodes[index - 1]);
-              }
-
-              // move forward
-              else if (value.isNotEmpty) {
-                if (index < widget.numberOfTxtFeilds - 1) {
-                  FocusScope.of(context).requestFocus(focusNodes[index + 1]);
-                } else {
-                  FocusScope.of(context).unfocus();
+              onChanged: (value) {
+                // forward move
+                if (value.isNotEmpty) {
+                  if (index < widget.numberOfTxtFeilds - 1) {
+                    FocusScope.of(context)
+                        .requestFocus(focusNodes[index + 1]);
+                  } else {
+                    FocusScope.of(context).unfocus();
+                  }
                 }
-              }
-            },
+
+                // backward move (only focus)
+                else {
+                  if (index > 0) {
+                    FocusScope.of(context)
+                        .requestFocus(focusNodes[index - 1]);
+                  }
+                }
+
+                _notify();
+              },
+            ),
           ),
         );
       }),
