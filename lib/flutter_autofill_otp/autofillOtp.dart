98d4122fd1bc389aafa9041827758914f0c25168
AkshaySarkale <akshaysarkale999@gmail.com>
2026-05-29 09:45:34 +0530

    added autotextbox in otp functionally

--- lib/flutter_autofill_otp/autofillOtp.dart
+++ lib/flutter_autofill_otp/autofillOtp.dart
@@ -1,19 +1,36 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-class Autofillotp extends StatelessWidget {
-  final TextEditingController txtCtrl;
-  const Autofillotp({super.key,required this.txtCtrl});
+
+class Autofillotp extends StatefulWidget {
+  final int numberOfTxtFeilds;
+
+  const Autofillotp({super.key, required this.numberOfTxtFeilds});
 
   @override
-  Widget build(BuildContext context) {
-    return TextFormField(
-      controller: txtCtrl,
-      maxLength: 1,
-      decoration: InputDecoration(
-        border: OutlineInputBorder(
+  State<Autofillotp> createState() => _AutofillotpState();
+}
 
-        )
-      ),
+class _AutofillotpState extends State<Autofillotp> {
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: List.generate(widget.numberOfTxtFeilds, (index) {
+        return SizedBox(
+          width: 50,
+          child: TextField(
+            keyboardType: TextInputType.number,
+            textAlign: TextAlign.center,
+            maxLength: 1,
+            decoration: const InputDecoration(counterText: ""),
+            onChanged: (value) {
+              if (value.isNotEmpty && index < widget.numberOfTxtFeilds) {
+                FocusScope.of(context).nextFocus();
+              }
+            },
+          ),
+        );
+      }),
     );
   }
 }
